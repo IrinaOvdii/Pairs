c135e43242d29b25496c15bcb2e5cba13114f206
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-29 17:59:25 +0100

    Add API users&pairs index

--- app/controllers/api/pairs_controller.rb
+++ app/controllers/api/pairs_controller.rb
@@ -1,5 +1,17 @@
-def index
-    render status: 200, json: {
-      pairs: Pair.all
-    }.to_json
+class Api::PairsController < ApplicationController
+  def index
+    if current_user.admin
+      render status: 200, json: {
+        pairs: Pair.all
+      }.to_json
+    elsif current_user.match_pairs.count > 0
+      render status: 200, json: {
+        pairs: current_user.match_pairs
+      }.to_json
+    else
+      render status: 200, json: {
+        pairs: current_user.student_pairs
+      }.to_json
+    end
   end
+end
