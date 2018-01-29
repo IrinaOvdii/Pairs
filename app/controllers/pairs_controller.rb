eddc20b86596bbc5fe1a192ce0f377ddbda919d4
Erlemorgaine <erle_3@hotmail.com>
2018-01-29 17:49:56 +0100

    fix routes

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -4,17 +4,11 @@ def index
 
   if current_user.admin
     @pairs = Pair.all
+  elsif current_user.match_pairs.count > 0
+    @pairs =  current_user.match_pairs
   else
-    @pairs = Pair.find_all_user
+    @pairs =  current_user.student_pairs
   end
-
-
-
-  # elsif current_user.match_pairs.count > 0
-  #   @pairs =  current_user.match_pairs
-  # else
-  #   @pairs =  current_user.student_pairs
-  # end
 end
 
 
