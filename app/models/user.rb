6c16c53d2aa5af67f179f0c4f4962903595711a5
Erlemorgaine <erle_3@hotmail.com>
2018-01-29 14:46:52 +0100

    Add to associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :student_pairs, :class_name => 'Pair', :foreign_key => 'student_id'
   has_many :match_pairs, :class_name => 'Pair', :foreign_key => 'match_id'
 
-  #has_many :students, through: :match_pairs, source: :user
-  #has_many :matches, through: :student_pairs, source: :user
+  has_many :users, through: :match_pairs, source: :student
+  has_many :users, through: :student_pairs, source: :match
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180129115112) do
 
   # These are extensions that must be enabled in order to support this database
@@ -38,8 +37,8 @@ ActiveRecord::Schema.define(version: 20180129115112) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
     t.string "name"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
