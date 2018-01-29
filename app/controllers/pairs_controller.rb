68f91ed5269df91e9d628bc1a9c9424a8f533e76
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-29 17:39:29 +0100

    Create api

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,13 +1,20 @@
 class PairsController < ApplicationController
 
 def index
+
   if current_user.admin
     @pairs = Pair.all
-  elsif current_user.match_pairs.count > 0
-    @pairs =  current_user.match_pairs
   else
-    @pairs =  current_user.student_pairs
+    @pairs = Pair.find_all_user
   end
+
+
+
+  # elsif current_user.match_pairs.count > 0
+  #   @pairs =  current_user.match_pairs
+  # else
+  #   @pairs =  current_user.student_pairs
+  # end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
   root to: 'pages#home'
-
   devise_for :users
-
   resources :pairs
+
+  namespace :api do
+    root to: 'pages#home'
+    devise_for :users
+    resources :pairs
+  end
 end
