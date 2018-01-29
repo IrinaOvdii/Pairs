bf2d2d54304a0d93e9c51ce1538466a4e4c6c4aa
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-29 15:12:11 +0100

    Create def home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    if current_user.admin?
+      @pairs = Pair.all
+    else
+      @pair =  current_user.student_pairs
+    end
   end
+
+
 end
