4db8f4b11946a0069079f2d245fcbe4f03a12cf4
Erlemorgaine <erle_3@hotmail.com>
2018-01-30 14:57:48 +0100

    commit just to be sure

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :pairs
-  resources :users, only: [:index]
-  
+  resources :users, only: [:index, :edit, :update]
+
   namespace :api do
     root to: 'pages#home'
     devise_for :users
-    resources :users, only: [:index]
+    resources :users, only: [:index, :edit, :update]
     resources :pairs
   end
 end
